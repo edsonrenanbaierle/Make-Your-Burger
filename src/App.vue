f86d8759ccf9a37ff8715d0afda14b33f725348b
Edson Baierle <edsonbaierle@gmail.com>
2023-12-27 10:30:39 -0300

    Make_you_Burger
    Fazendo criação do projeto para aprendizagem de vue, com header e footer prontos.

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <Navbar :logo="src_logo" :alt="App_name"/>
+  <router-view/>
+  <FooterVue/>
+</template>
+
+
+<script>
+import FooterVue from './components/Footer.vue';
+import Navbar from './components/Navbar.vue';
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    FooterVue
+  },
+  data(){
+    return {
+      src_logo: '/img/logo.png',
+      App_name: 'Make Your Burger'
+    }
+  }
+}
+</script>
+
+
+<style>
+  *{
+    font-family: Helvetica, Arial, sans-serif;
+    padding: 0;
+    margin:0;
+    box-sizing: border-box;
+  }
+
+  .main-container {
+    margin: 50px;
+    min-height: 250px;
+  }
+
+  h1 {
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <Banner/>
+  <div class="main-container">
+    <h1>Monte o seu burger:</h1>
+  </div>
+</template>
+
+<script>
+import Banner from '@/components/Banner.vue';
+export default {
+  name: 'Home',
+  components: {
+    Banner
+  }
+}
+</script>
